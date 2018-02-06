9c3b2eff01a6ae983a2238cbdb80719bd028e151
lamwyman <wyman95@gmail.com>
2018-02-06 22:23:40 +0800

    Add files via upload
    Original copy of the homepage

---
+++ js/datetime.js
@@ -0,0 +1,26 @@
+function startTime() {
+	var today = new Date();
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById('time').innerHTML =
+	h + ":" + m + ":" + s;
+	var tmp = h + ":" + m + ":" + s;
+	
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+	var curWeekDay = days[today.getDay()];
+	var curDay = today.getDate();
+	var curMonth = months[today.getMonth()];
+	var curYear = today.getFullYear();
+	var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
+	document.getElementById("date").innerHTML = date;
+	
+	var t = setTimeout(startTime, 5);
+}
+function checkTime(i) {
+	if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+	return i;
+}
