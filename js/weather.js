8126fa20b0bd52e91b32e8cc404dcf357e9c5a73
wyman <wyman95@gmail.com>
2018-02-07 22:14:07 +0800

    Layout redesigned

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,5 @@
 function getweather(){
-    var city = "Tung Chung, HK";
+    var city = "Kowloon, HK";
     var searchtext = "select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u='c'"
     //change city variable dynamically as required
     $.getJSON("https://query.yahooapis.com/v1/public/yql?q=" + searchtext + "&format=json").success(function(data){
