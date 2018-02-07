9e7c5fc5f966a365c62a34f53b97e1e2d30e0437
wyman <wyman95@gmail.com>
2018-02-07 21:03:12 +0800

    successfully get data from yahoo weather

---
+++ js/weather.js
@@ -0,0 +1,9 @@
+function getweather(){
+    var city = "Tung Chung, HK";
+    var searchtext = "select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u='c'"
+    //change city variable dynamically as required
+    $.getJSON("https://query.yahooapis.com/v1/public/yql?q=" + searchtext + "&format=json").success(function(data){
+     console.log(data);
+     $('#weather_text').html("Temperature in " + city + " is " + data.query.results.channel.item.condition.temp + "°C");
+    });
+}
