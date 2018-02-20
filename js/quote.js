280795353a96781c7e3e0fa8b8724c8c6e276db1
GloryYYC <yycglory@gmail.com>
2018-02-20 12:43:48 +0800

    combine all js

--- js/quote.js
+++
@@ -1,3 +0,0 @@
-$.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-  $("#quote").append(a[0].content + "<p>— " + a[0].title + "</p>")
-});
--- js/random_background.js
+++
@@ -1,2 +0,0 @@
-// document.getElementById("bgimg").style.backgroundImage = 'url('+"https://source.unsplash.com/"+screen.width+"x"+screen.height+')';
-$('body').css('background-image', 'url('+"https://source.unsplash.com/"+screen.width+"x"+screen.height+')');
