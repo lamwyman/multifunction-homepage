003fa95676640c5ea372d7bc6b69e9beb3d8374c
wyman <wyman95@gmail.com>
2018-02-21 20:40:51 +0800

    Added weather detail

--- js/main.js
+++ js/main.js
@@ -48,15 +48,23 @@ function getWeather() {
 		var city = data.query.results.channel.location.city;
 		var temp = data.query.results.channel.item.condition.temp;
 		var code = data.query.results.channel.item.condition.code;
+		var forecast = data.query.results.channel.item.forecast;
 
 		$('#weatherText').html(city + " " + temp + "°C" + " ");
 		$('#weatherText').append("<i class='wi wi-yahoo-"+code+"'></i>"); 
 		//code api
 		//https://erikflowers.github.io/weather-icons/api-list.html
 
-		$('#weatherDetail').html("sunrise: " + data.query.results.channel.astronomy.sunrise);
-		$('#weatherDetail').append("<br>");
-		$('#weatherDetail').append("sunset: " + data.query.results.channel.astronomy.sunset);
+		$('#wdhead').append("<i class='wi wi-yahoo-"+code+"'></i>"); 
+		$('#wdhead').append(data.query.results.channel.item.condition.text);
+
+		var tmp = ["#wd1","#wd2","#wd3","#wd4","#wd5"];
+		for(var i=0;i<5; i++){
+			$(tmp[i]).append(forecast[i].day + "<br>");
+			$(tmp[i]).append("High: " + forecast[i].high + "°C" + "<br>");
+			$(tmp[i]).append("Low: " + forecast[i].low + "°C" + "<br>");
+		}
+
 	});
 }
 
